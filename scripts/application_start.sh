2df364c0689c27da794de631f780baa5fd5cf1d6
Viktor Volokhan <voloxanvitya@gmail.com>
2025-02-07 14:25:42 +0200

    fix: scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,16 +4,8 @@
 sudo chmod -R 777 /home/ubuntu/aucar
 
 #navigate into our working directory where we have all our github files
-cd /home/ubuntu/aucar
-
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm" 
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm 
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
-#install node modules
-cd server
+cd /home/ubuntu/aucar/server
 npm install
 
-#start our node app in the background
-npm start > app.out.log 2> app.err.log < /dev/null & 
+npm run build
+sudo systemctl start myapp.service
